7e2fb30cf3206955ccafa110829ad82f1b3aa151
asma laptop <asmaalashwali@hotmail.com>
2023-09-27 21:52:31 +0300

    first component added

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+  // js object
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      p {
+        color: brown;
+      }
+    `,
+  ],
+})
+export class ServerComponent {}
