d5b9b58ec744c6e6ee7e6a48f0f4a6a2ff4df029
asma laptop <asmaalashwali@hotmail.com>
2023-10-05 11:41:54 +0300

    assignment 3 added

---
+++ Assignment3/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  showPassword = false;
+  log = [];
+  onToggleDetails() {
+    this.showPassword = !this.showPassword;
+    this.log.push(this.log.length + 1);
+  }
+}
